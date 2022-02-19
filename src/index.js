f1eb2694d2fa9cbc1eb3405ea70b518a3baef969
MiCetF <webmaster@micetf.fr>
2022-02-19 13:59:32 +0100

    Mise en place de babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("ok");
+const papa = {
+    name: "Fred",
+    age: 57,
+};
+const maman = {
+    name: "Choube",
+    age: 49,
+};
+const lisa = {
+    name: "Lisa",
+    age: 21,
+};
+const fany = {
+    name: "Fany",
+    age: 18,
+};
+
+const famille = [
+    {
+        ...papa,
+        type: "papa",
+    },
+    {
+        ...maman,
+        type: "maman",
+    },
+];
+
+document.querySelector("#app").innerHTML = famille;
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,23 @@ const navigateur = require("./navigateur.config");
 
 module.exports = {
     mode: "development",
+    devtool: false,
     devServer: {
         static: "./dist",
         open: navigateur,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+        ],
     },
 };
